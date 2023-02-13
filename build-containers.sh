9b6020b65f674a2567eb23fe53bf4f7b01cf07f6
Edwin Torres <codesandtags@gmail.com>
2023-02-12 21:17:20 -0500

    docs: Updated docker scripts

--- build-containers.sh
+++ build-containers.sh
@@ -1,43 +1,65 @@
 # Build all Docker containers
 
 # Posts Service
-echo "📦 Building Posts"
+POST_IMAGE="posts"
+echo "\n📦 Building [$POST_IMAGE]"
 cd posts/
 docker build -t codesandtags/posts:0.0.1 .
+echo "\n📪 Pushing [$POST_IMAGE] to Docker Hub"
+docker push codesandtags/posts:0.0.1
+#echo "\n🚨 Running Security Scan for [$POST_IMAGE] image"
+#docker scan codesandtags/posts:0.0.1
 cd ..
 
 # Comments Service
-echo "📦 Building Comments"
+COMMENTS_IMAGE="comments"
+echo "\n📦 Building [$COMMENTS_IMAGE]"
 cd comments/
 docker build -t codesandtags/comments:0.0.1 .
+echo "\n📪 Pushing [$COMMENTS_IMAGE] to Docker Hub"
+docker push codesandtags/comments:0.0.1
+#echo "\n🚨 Running Security Scan for [$COMMENTS_IMAGE] image"
+#docker scan codesandtags/comments:0.0.1
 cd ..
 
 
 # Moderation Service
-echo "📦 Building Moderation"
+MODERATION_IMAGE="query"
+echo "\n📦 Building [$MODERATION_IMAGE]"
 cd moderation/
 docker build -t codesandtags/moderation:0.0.1 .
+echo "\n📪 Pushing [$MODERATION_IMAGE] to Docker Hub"
+docker push codesandtags/moderation:0.0.1
 cd ..
 
 
 # Query Service
-echo "📦 Building Query"
+QUERY_IMAGE="query"
 cd query/
+echo "\n📦 Building [$QUERY_IMAGE]"
 docker build -t codesandtags/query:0.0.1 .
+echo "\n📪 Pushing [$QUERY_IMAGE] to Docker Hub"
+docker push codesandtags/query:0.0.1
 cd ..
 
 
 # Event Bus Service
-echo "📦 Building Event Bus"
+EVENT_BUS_IMAGE="event-bust"
+echo "\n📦 Building [$EVENT_BUS_IMAGE]"
 cd event-bus/
 docker build -t codesandtags/event-bus:0.0.1 .
+echo "\n📪 Pushing [$EVENT_BUS_IMAGE] to Docker Hub"
+docker push codesandtags/event-bus:0.0.1
 cd ..
 
 
 # Client Web App
-echo "📦 Building Client Web App"
+WEB_IMAGE="web-app"
+echo "\n📦 Building [$WEB_IMAGE]"
 cd client/
 docker build -t codesandtags/client:0.0.1 .
+echo "\n📪 Pushing [$WEB_IMAGE] to Docker Hub"
+docker push codesandtags/client:0.0.1
 cd ..
 
-echo "Docker containers generated. 🍻"
+echo "\nDocker containers generated. 🍻"
